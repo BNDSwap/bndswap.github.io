1590d2c5028190452928adb046580c4e773b1450
DeFiDev <defidev2021@gmail.com>
2021-08-20 02:17:38 -0700

    init

---
+++ precache-manifest.a78c1e660ac9fc00763b811b3867f64b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8488be5a61e7cb1f2b74",
+    "url": "/static/css/main.e8f97300.chunk.css"
+  },
+  {
+    "revision": "8488be5a61e7cb1f2b74",
+    "url": "/static/js/main.8488be5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "62f951361f6089f328ea",
+    "url": "/static/css/2.69503456.chunk.css"
+  },
+  {
+    "revision": "62f951361f6089f328ea",
+    "url": "/static/js/2.62f95136.chunk.js"
+  },
+  {
+    "revision": "60ea39b1b3c31b0ec8502386c8ed3bf1",
+    "url": "/static/media/wp.60ea39b1.pdf"
+  },
+  {
+    "revision": "6c407ce55980f106983ff6c05352de25",
+    "url": "/static/media/audit.6c407ce5.pdf"
+  },
+  {
+    "revision": "afa08cd95ca924af2bfb30d0d67ec6b1",
+    "url": "/static/media/eth.afa08cd9.png"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "/static/media/mdx.8e72f6f2.png"
+  },
+  {
+    "revision": "2d81a68f2bba32dd3c6abd4446ab5b6d",
+    "url": "/static/media/nft.2d81a68f.png"
+  },
+  {
+    "revision": "9fba729c1b3ee097bcee875ddc22895f",
+    "url": "/static/media/nsp.9fba729c.png"
+  },
+  {
+    "revision": "32accecc5c52269db9cb78d1bf0f5fca",
+    "url": "/static/media/miningRule.32accecc.pdf"
+  },
+  {
+    "revision": "675cf551a80cd9022674c69e31f8550d",
+    "url": "/static/media/logo.675cf551.png"
+  },
+  {
+    "revision": "ca57ef50388b71d453fbeceb951cc8b1",
+    "url": "/static/media/certificate.ca57ef50.pdf"
+  },
+  {
+    "revision": "214c0376562b73d72cf84d8576a6dcc5",
+    "url": "/static/media/bg.214c0376.jpg"
+  },
+  {
+    "revision": "e2492c572295e994065429ae3f437564",
+    "url": "/index.html"
+  }
+];
